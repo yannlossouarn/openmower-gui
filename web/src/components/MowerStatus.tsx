dde18ee8a7c8564e3e3c504d27f4efaa515c193e
Yann Lossouarn <yann@lossouarn.net>
2024-05-29 17:53:34 +0200

    Slight improvement of the MowerStatus bar

--- web/src/components/MowerStatus.tsx
+++ web/src/components/MowerStatus.tsx
@@ -1,21 +1,121 @@
-import {useHighLevelStatus} from "../hooks/useHighLevelStatus.ts";
-import {Col, Row, Statistic} from "antd";
-import {PoweroffOutlined, WifiOutlined} from "@ant-design/icons"
-import {progressFormatterSmall, stateRenderer} from "./utils.tsx";
+import { useHighLevelStatus } from "../hooks/useHighLevelStatus.ts";
+import { useGPS } from "../hooks/useGPS.ts";
+import { Col, Row, Statistic } from "antd";
+
+import { BatteryCharge, BatteryLow, BatteryMid, BatteryHigh, BatteryFull} from "./IconBatteryComponent.tsx";
+import { MapPin, MapPinCheck, MapPinApproximation, MapPinOff, BroadcastTower, BroadcastTowerOff} from "./IconGeolocationComponent.tsx";
+import { stateRenderer } from "./utils.tsx";
+import { AbsolutePoseFlags as Flags } from "../types/ros.ts";
+import { useEffect, useState } from "react";
+
+const getBatteryIcon = (batteryLevel: number) => {
+  if (batteryLevel > 0.9) return <BatteryFull />;
+  if (batteryLevel > 0.66) return <BatteryHigh />;
+  if (batteryLevel > 0.33) return <BatteryMid />;
+  if (batteryLevel < 0.33) return <BatteryLow />;
+  return <BatteryLow stroke={'grey'} fill={'grey'} />;
+};
+
+const getNextBatteryIcon = (batteryLevel: number) => {
+    if (batteryLevel > 0.9) return <BatteryCharge />;
+    if (batteryLevel > 0.66) return <BatteryFull />;
+    if (batteryLevel > 0.33) return <BatteryHigh />;
+    if (batteryLevel > 0.1) return <BatteryMid />;
+    return <BatteryLow stroke={'red'} fill={'red'} />;
+  };
+
+  const getRTKIcon = (flags: number, rtk: number) => {
+        if ((flags & rtk) !== 0) {
+            return <><BroadcastTower  {...{strokeWidth: '1', color: '#049F0B'}} /></>;
+        } else {
+            return <><BroadcastTowerOff  {...{strokeWidth: '1', color: '#EB0000'}} /></>;
+        }
+    }
+
+    const getGPSIcon = (flags: number) => {
+
+        if ((flags & Flags.FIXED) != 0) {
+          // fixType = "FIX";
+          return <><MapPinCheck {...{strokeWidth: '1', color: '#049F0B'}} /></>;
+        } else if ((flags & Flags.FLOAT) != 0) {
+          // fixType = "FLOAT";
+          return <><MapPinApproximation /></>;
+        } else {
+            return <><MapPinOff /></>;
+        }
+    }
 
 export const MowerStatus = () => {
-    const {highLevelStatus} = useHighLevelStatus();
-    return <Row gutter={[16, 16]} style={{margin: 0}}>
-        <Col><Statistic valueStyle={{color: "#3f8600", fontSize: "14px"}}
-                        value={stateRenderer(highLevelStatus.StateName)}/></Col>
-        <Col><Statistic
-            prefix={<WifiOutlined style={{color: (highLevelStatus.GpsQualityPercent ?? 0) > 0 ? "green" : "red"}}/>}
-            valueStyle={{fontSize: "14px"}} precision={0}
-            value={(highLevelStatus.GpsQualityPercent ?? 0) * 100}
-            suffix={"%"}/></Col>
-        <Col><Statistic prefix={<PoweroffOutlined style={{color: highLevelStatus.IsCharging ? "green" : undefined}}/>}
-                        valueStyle={{fontSize: "14px"}} precision={2}
-                        value={(highLevelStatus.BatteryPercent ?? 0) * 100}
-                        formatter={progressFormatterSmall}/></Col>
-    </Row>;
-}
+  const { highLevelStatus } = useHighLevelStatus();
+  const gps = useGPS();
+
+  const flags = gps.Flags ?? 0;
+  let fixType = "\u2013";
+  if ((flags & Flags.FIXED) != 0) {
+    fixType = "FIX";
+  } else if ((flags & Flags.FLOAT) != 0) {
+    fixType = "FLOAT";
+  }
+
+  const batteryCharging = highLevelStatus.IsCharging;
+  const batteryLevel = highLevelStatus.BatteryPercent ?? 0;
+  const [animationStep, setAnimationStep] = useState(0);
+
+  useEffect(() => {
+    if (batteryCharging) {
+      const interval = setInterval(() => {
+        setAnimationStep((prev) => (prev === 0 ? 1 : 0));
+      }, 500); // Change the interval duration as needed
+      return () => clearInterval(interval);
+    }
+  }, [batteryCharging]);
+
+  let batteryIcon;
+  if (batteryCharging) {
+    batteryIcon = (
+      <>
+        {animationStep === 0 ? getBatteryIcon(batteryLevel) : getNextBatteryIcon(batteryLevel)}
+      </>
+    );
+  } else {
+    batteryIcon = getBatteryIcon(batteryLevel);
+  }
+
+    let RTKIcon = getRTKIcon(flags, Flags.RTK);
+
+    let GPSIcon = getGPSIcon(flags);
+
+  return (
+    <Row gutter={[16, 16]} style={{ margin: 0 }}>
+      <Col>
+        <Statistic
+          valueStyle={{ color: "#3f8600", fontSize: "16px" }}
+          value={stateRenderer(highLevelStatus.StateName)}
+        />
+      </Col>
+      <Col>
+        <>
+        {RTKIcon}
+        </>
+      </Col>
+      <Col>
+        <Statistic
+          prefix={GPSIcon}
+          valueStyle={{ fontSize: "13px" }}
+          precision={1}
+          value={gps.PositionAccuracy !== undefined ? gps.PositionAccuracy * 100 : "?"}
+          suffix={"cm"}
+        />
+      </Col>
+      <Col>
+        <Statistic
+          prefix={<span style={{height: '24px'}}>{batteryIcon}</span>}
+          valueStyle={{ fontSize: "13px", height: "24px" }}
+          precision={0}
+          value={batteryLevel * 100}
+          suffix={"%"}
+        />
+      </Col>
+    </Row>
+  );
+};
