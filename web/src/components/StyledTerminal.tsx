5b996293322a6cd5e11f300e8da41c7e868e2eee
Yann Lossouarn <yann@lossouarn.net>
2025-11-06 16:30:09 +0100

    WIP : improve logs

--- web/src/components/StyledTerminal.tsx
+++ web/src/components/StyledTerminal.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const StyledTerminal = styled.div`
   div.react-terminal-wrapper {
     padding-top: 35px;
+    background-color: #1a1e24;
   }
 
   div.react-terminal-wrapper > div.react-terminal-window-buttons {
@@ -11,5 +12,10 @@ export const StyledTerminal = styled.div`
 
   div.react-terminal {
     height: auto;
+  }
+
+  div.react-terminal-light .react-terminal-line::before {
+    display: initial;
+    content: initial;
   }
 `;
